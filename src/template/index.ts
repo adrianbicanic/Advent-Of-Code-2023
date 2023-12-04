78d266ddcf86a9bb03549c75e476efdf5f9ae3dc
adrianbicanic <adibicanic@gmail.com>
2023-12-04 14:02:55 +0100

    Create challenge template

---
+++ src/template/index.ts
@@ -0,0 +1,9 @@
+import { join } from 'path';
+import { parseInput } from 'utils/file';
+
+const inputPath = join(__dirname, 'input.txt');
+const inputData = parseInput(inputPath);
+
+export function solve() {
+	return 0;
+}
